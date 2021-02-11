58b219a376c9be4dc2f3b849b5910ef4b7cfa2c0
Creator Piyush <piyushanand.1221@gmail.com>
2021-02-11 15:24:57 +0530

    Basic front page

---
+++ src/Pages/MainPage/MainPage.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import NavBar from "../../Components/Navbar/NavBar";
+import SideBar from "../../Components/SideBar/SideBar";
+import "./MainPage.css";
+
+export default function MainPage() {
+  return (
+    <div className="MainPage">
+      <div className="top">
+        <NavBar />
+      </div>
+      <div className="center">
+        <SideBar />
+        <div className="VideoView"></div>
+      </div>
+    </div>
+  );
+}
