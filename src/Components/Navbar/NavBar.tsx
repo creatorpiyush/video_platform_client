3067ee1ea1fb00bfb811c7e9ef8c0d28eca5c6ba
Creator Piyush <piyushanand.1221@gmail.com>
2021-02-11 16:52:22 +0530

    navbar done till search and logo

--- src/Components/Navbar/NavBar.tsx
+++ src/Components/Navbar/NavBar.tsx
@@ -1,9 +1,28 @@
 import React from "react";
+import "./NavBar.css";
+import PlayCircleOutlineIcon from "@material-ui/icons/PlayCircleOutline";
+import ReorderIcon from "@material-ui/icons/Reorder";
+import SearchIcon from "@material-ui/icons/Search";
 
 export default function NavBar() {
   return (
-    <div>
-      NavBar
-    </div>
+    <>
+      <div className="NavbarContainer">
+        <div className="left">
+          <button id="sidebarToggle">
+            <ReorderIcon id="icon" />
+          </button>
+          <PlayCircleOutlineIcon id="icon" style={{ color: "darkblue" }} />
+          <h1>StreamIt</h1>
+        </div>
+        <div className="center">
+          <input id="searchBar" type="text" placeholder="Search" />
+          <button id="searchButton">
+            <SearchIcon />
+          </button>
+        </div>
+        <div className="right"></div>
+      </div>
+    </>
   );
 }
